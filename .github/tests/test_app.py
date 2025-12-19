f6de015623060a129ecc19ee382162f520e40e80
lggerey228-sketch <lggerey228@gmail.com>
2025-12-19 21:15:57 +0300

    Update test_app.py

--- .github/tests/test_app.py
+++ .github/tests/test_app.py
@@ -1,5 +1,5 @@
 def test_home_endpoint():
-    from src.app import app
+    from app import app
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
